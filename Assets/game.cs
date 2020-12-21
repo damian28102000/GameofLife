b33a78c339928a60bd70b0cff4e3022ac2d7d501
damian28102000 <damiantomczak200000@gmail.com>
2020-12-21 15:02:52 +0100

    Initial commit

---
+++ Assets/game.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game : MonoBehaviour
+{
+    public GameObject prefab;
+    public List<GameObject> spawnees = new List<GameObject>();
+    int randomInt;
+
+    void Start()
+    {
+        for(int col =0, i =0;col<10;col++)
+        {
+            for(int row =0;row<10;row++,i++)
+            {
+                spawnees.Add(Instantiate(prefab, new Vector3(6*row, 0, 6*col), Quaternion.identity) as GameObject);
+                spawnees[i].GetComponent<organism>().isAlive = true;
+            }
+        }
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/organism.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class organism : MonoBehaviour
+{
+    public bool isAlive = false;
+
+    void Update()
+    {
+        if(isAlive)
+        {
+            GetComponent<Renderer>().material.color = new Color(0, 255, 0);
+        }
+        else
+        {
+            GetComponent<Renderer>().material.color = new Color(255, 0, 0);
+        }
+    }
+}
