cc019a124558c21f407c53476d34efb524df4aef
damian28102000 <damiantomczak200000@gmail.com>
2020-12-21 17:50:43 +0100

    almost

--- Assets/game.cs
+++ Assets/game.cs
@@ -5,24 +5,91 @@ using UnityEngine;
 public class game : MonoBehaviour
 {
     public GameObject prefab;
-    public List<GameObject> spawnees = new List<GameObject>();
-    int randomInt;
+    public GameObject[,] spawnees;
+    float lastTime;
 
     void Start()
     {
+        spawnees = new GameObject[10, 10];
         for(int col =0, i =0;col<10;col++)
         {
             for(int row =0;row<10;row++,i++)
             {
-                spawnees.Add(Instantiate(prefab, new Vector3(6*row, 0, 6*col), Quaternion.identity) as GameObject);
-                spawnees[i].GetComponent<organism>().isAlive = true;
+                spawnees[row,col] = Instantiate(prefab, new Vector3(6*row, 0, 6*col), Quaternion.identity);
             }
+        }
+
+        for(int i =0;i<10;i++)
+        {
+            spawnees[Random.Range(0,10), Random.Range(0, 10)].GetComponent<organism>().isAlive = true;
         }
 
     }
 
     void Update()
     {
-        
+        float currentTime = Time.time;
+        Debug.Log(currentTime - lastTime);
+        if (currentTime - lastTime >=1)
+        {
+            for (int col = 0, i = 0; col < 10; col++)
+            {
+                for (int row = 0; row < 10; row++, i++)
+                {
+                    if (row != 9)
+                    {
+                        // Check if the right neighbor is alive
+                        if (spawnees[row + 1, col].GetComponent<organism>().isAlive == true)
+                        {
+                            spawnees[row, col].GetComponent<organism>().neigbours++;
+                        }
+                    }
+
+                    if (row != 0)
+                    {
+                        // Check if the left neighbor is alive
+                        if (spawnees[row - 1, col].GetComponent<organism>().isAlive == true)
+                        {
+                            spawnees[row, col].GetComponent<organism>().neigbours++;
+                        }
+                    }
+
+                    if (col != 9)
+                    {
+                        // Check if the up neighbor is alive
+                        if (spawnees[row, col + 1].GetComponent<organism>().isAlive == true)
+                        {
+                            spawnees[row, col].GetComponent<organism>().neigbours++;
+                        }
+                    }
+
+                    if (col != 0)
+                    {
+                        // Check if the bottom neighbor is alive
+                        if (spawnees[row, col - 1].GetComponent<organism>().isAlive == true)
+                        {
+                            spawnees[row, col].GetComponent<organism>().neigbours++;
+                        }
+                    }
+                }
+            }
+
+            for (int col = 0, i = 0; col < 10; col++)
+            {
+                for (int row = 0; row < 10; row++, i++)
+                {
+                    if (spawnees[row, col].GetComponent<organism>().neigbours == 2 || spawnees[row, col].GetComponent<organism>().neigbours == 3)
+                    {
+                        spawnees[row, col].GetComponent<organism>().isAlive = true;
+                    }
+                    else
+                    {
+                        spawnees[row, col].GetComponent<organism>().isAlive = false;
+                    }
+                }
+            }
+
+            lastTime = currentTime;
+        }
     }
 }
--- Assets/organism.cs
+++ Assets/organism.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class organism : MonoBehaviour
 {
     public bool isAlive = false;
-
+    public int neigbours;
     void Update()
     {
         if(isAlive)
